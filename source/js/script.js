e585fe7924a37f1d9a7762debcc82f4c76111e4a
Kalaganov5 <kalaganov5@gmail.com>
2020-12-17 14:52:33 +0300

    Работа с меню и доработка блока

--- source/js/script.js
+++ source/js/script.js
@@ -4,15 +4,14 @@
 let menuToggleButton = document.querySelector(".page-header__toggle");
 let menu = document.querySelector(".page-header__nav");
 let menuClosed = document.querySelector(".page-header__nav--closed");
+let header = document.querySelector(".page-header");
 
 // Close menu onload js
-
-  menu.classList.add("page-header__nav--closed");
-
-
+menu.classList.add("page-header__nav--closed");
 
 // Open and closed button and menu
 menuToggleButton.onclick = function() {
+  header.classList.toggle("page-header--menu-open");
   menuToggleButton.classList.toggle("page-header__toggle--hamburger");
   menu.classList.toggle("page-header__nav--closed");
 };
