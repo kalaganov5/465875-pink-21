f30d3c35a7b8ca0862a00ae7f1ac4c51298fb329
Kalaganov5 <kalaganov5@gmail.com>
2020-12-23 15:23:58 +0300

    Готовое меню с учетов тз и js

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,17 @@
 
 // variables
 let menuToggleButton = document.querySelector(".page-header__toggle");
-let menu = document.querySelector(".page-header__nav");
 let menuClosed = document.querySelector(".page-header__nav--closed");
 let header = document.querySelector(".page-header");
 
-// Close menu onload js
-menu.classList.add("page-header__nav--closed");
+// show .page-header__toggle only support js
+menuToggleButton.classList.add("support-js");
+
+// hide menu with support js
+header.classList.add("page-header--closed");
 
 // Open and closed button and menu
 menuToggleButton.onclick = function() {
-  header.classList.toggle("page-header--menu-open");
   menuToggleButton.classList.toggle("page-header__toggle--hamburger");
-  menu.classList.toggle("page-header__nav--closed");
+  header.classList.toggle("page-header--closed");
 };
