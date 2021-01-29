0cbcce32892fe64f01223c9871f53bf888f10233
Kalaganov5 <kalaganov5@gmail.com>
2021-01-29 23:03:20 +0300

    Публикация проекта

---
+++ build/js/script.js
@@ -0,0 +1,58 @@
+"use strict";
+// HEADER
+// variables
+let menuToggleButton = document.querySelector(".page-header__toggle");
+let menuClosed = document.querySelector(".page-header__nav--closed");
+let header = document.querySelector(".page-header");
+
+// show .page-header__toggle only support js
+menuToggleButton.classList.add("page-header--support-js");
+
+// hide menu with support js
+header.classList.add("page-header--closed");
+
+// Open and closed button and menu
+menuToggleButton.onclick = function(evt) {
+  evt.preventDefault();
+  menuToggleButton.classList.toggle("page-header__toggle--hamburger");
+  header.classList.toggle("page-header--closed");
+};
+
+// popup
+document.addEventListener("DOMContentLoaded", function() {
+  let submitForm = document.querySelector(".competition__form ");
+  let popup = document.querySelector(".popup-form");
+  let popupClose = document.querySelectorAll(".popup-form__button");
+  let buttonSubmit = document.querySelector(".form__submit");
+
+  buttonSubmit.onclick = function(evt) {
+    if(submitForm.checkValidity() === false) {
+      evt.preventDefault();
+      popup.classList.add("popup-form--active-form-error");
+    }
+  }
+
+  submitForm.onsubmit = function (evt) {
+    evt.preventDefault();
+    popup.classList.add("popup-form--active-form-send");
+    submitForm.reset()
+  };
+
+  for (let i = 0; i <= popupClose.length; i++) {
+    popupClose[i].onclick = function(evt) {
+      evt.preventDefault();
+      if (popup.classList.contains("popup-form--active-form-error")) {
+        popup.classList.remove("popup-form--active-form-error");
+        submitForm.reportValidity()
+      }
+      if (popup.classList.contains("popup-form--active-form-send")) {
+        popup.classList.remove("popup-form--active-form-send");
+      }
+    };
+  }
+});
+
+
+// map
+let mapBlock = document.querySelector(".footer__map");
+mapBlock.classList.add("footer__map-js");
